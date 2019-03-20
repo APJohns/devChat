c15f57fde7633c1d38f1d0c110cb8d668f437b2f
Ashley Johns <ashleypjohns@gmail.com>
2019-03-19 22:36:30 -0400

    Create input which updates state with messages

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
 
 class App extends Component {
+	messageRef = React.createRef();
+	messageFormRef = React.createRef();
+
+	state = {
+		messages: {}
+	};
+
+	sendMessage = e => {
+		e.preventDefault();
+
+		let messages = { ...this.state.messages };
+		messages[Date.now()] = this.messageRef.current.value;
+		this.setState({ messages });
+
+		this.messageFormRef.current.reset();
+	};
+
 	render() {
-		return <div className="App" />;
+		return (
+			<main>
+				<h1>DevChat</h1>
+				<section className="messages">
+					{Object.keys(this.state.messages).map(key => (
+						<p key={key}>{this.state.messages[key]}</p>
+					))}
+				</section>
+				<form ref={this.messageFormRef} onSubmit={this.sendMessage}>
+					<input type="text" ref={this.messageRef} />
+				</form>
+			</main>
+		);
 	}
 }
 
